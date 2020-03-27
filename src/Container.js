5fde6b7cfb22797a71053bcbdaae4d6b284c3be5
Salim Mayan <mailsalim@gmail.com>
2020-03-27 16:06:40 -0700

    Fixed display of chosen check box names

--- src/Container.js
+++ src/Container.js
@@ -15,67 +15,9 @@ export class Container {
         this.title = title;
         this.mediaType = mediaType;
         this.href = href;
-    }    
-
-    createOuputContent(index, checkBoxArray, containerArray){
-        console.log(`inside createOuputContent: index is ${index} , array is checkBoxArray`);
-        let outputArray = [];
-        let image = `<figure><img src="${containerArray[index].href}" alt="${containerArray[index].description}" class="img-fluid"></figure>`;
-        outputArray.push(image);
-        for (let i = 0; i < checkBoxArray.length; i++) {
-            if (checkBoxArray[i] === "title"){
-                $('.showTitle').empty().text(`<span class="coralColor">Title: </spn>${containerArray[index].title}`);
-                outputArray.push(image);
-            }
-            else if (checkBoxArray[i] === "dateCreated"){
-                $('.showDateCreated').empty().text(`Date Created: ${containerArray[index].dateCreated}`);
-                outputArray.push(image);
-            }
-            else if (checkBoxArray[i] === "description"){
-                $('.showDescription').empty().text(`Description: ${containerArray[index].description}`);
-                outputArray.push(image);
-            }
-            else if (checkBoxArray[i] === "href"){
-                $('.showHref').empty().text(`Image URL is ${containerArray[index].href}`);
-                outputArray.push(image);
-            }
-                
-            return outputArray;
-        }
-        
-        
-       
-       
-        
-
-        
-
     }
 
-    calculatePrice (pizzaSize, pizzaToppings) {
-        var totalPrice = 0;
-        var sizePrice = 0;
-        switch (this.pizzaSize) {
-          case 'ExtraLarge':
-            sizePrice = 14;
-            //console.log('Loop: ExtraLarge Pizza Costs $14.');
-            break;
-          case 'Large':
-            sizePrice = 12;
-            //console.log('Loop:Large Pizza Costs $12.');
-            break;
-          case 'Medium':
-            sizePrice = 10;
-            //console.log('Loop:Medium Pizza Costs $10.');
-            break;
-          case 'Small':
-            sizePrice = 8;
-            //console.log('Loop:Small Pizza Costs $8.');
-            break;
-        }
-        this.totalPrice = sizePrice + (this.pizzaToppings.length * 1);
-        //console.log(`Length:Calculated price::${this.pizzaToppings.length}:${this.totalPrice}`);
-        return this.totalPrice;
-      }
+
+
 
 }
--- src/main.js
+++ src/main.js
@@ -20,8 +20,7 @@ $(function () {
 $("select#dDId").on('change',function(){
   console.log(`Inside drop down disable loop`);
   let temp = $("#dDId option:selected").text();
-  console.log(`salim option value is ${temp}`);
-  
+  console.log(`salim option value is ${temp}`);  
   if(temp ==="Select") {
       $("button#submit").attr('disabled',true);
       console.log(`Inside drop down disable loop - inside IF`);
@@ -31,22 +30,6 @@ $("select#dDId").on('change',function(){
       console.log(`Inside drop down disable loop - inside ELSE`);
   }
 });
-
-
-// $(function () {
-//   $("select#dDId").on('change', function () {
-//     console.log(`Inside drop down disable loop`);
-//     if ($(this).find('option:selected').text() == "Select") {
-//       console.log(`Inside drop down disable loop - inside IF`);
-//       $("button#submit").attr('disabled', true);
-//       bt.disabled = true;  
-//   }
-//     else {
-//       $("button#submit").attr('disabled', false);
-//     }
-//   });
-// });  
-
 
 $(document).ready(function () {
   var buttonObj = document.querySelector("button");
@@ -58,22 +41,21 @@ $(document).ready(function () {
   });
   $('#formOne').submit(function () {
     $('#results').empty();
-    // var buttonObj = document.querySelector("button");
-    // buttonObj.textContent = "submitted";
     event.preventDefault();
     $("#toppingsDiv").hide();
+    $("#metaData").hide();
     $("#previous").show();
     $("#next").show();
     let userEnteredMetaData = $(".metaData").val();
     //   $('input[type="number"], textarea').val('');  // to clear form of entered value after submit
-    let metaDataSelection = [];
+    var metaDataSelection = [];
 
     $("input:checkbox[name=metaData]:checked").each(function () {
       metaDataSelection.push($(this).val());
     });
     $("input[type=checkbox]").each(function () { this.checked = false; }); //to uncheck previously checked checkboxes
 
-    console.log(`metaDataSelection is ${metaDataSelection}`);
+    console.log(`Main: metaDataSelection is ${metaDataSelection}`);
 
     let promise = new Promise(function (resolve, reject) {
       //const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
@@ -90,15 +72,14 @@ $(document).ready(function () {
           reject(Error(request.statusText));
         }
       }
-
     });
 
     promise.then(function (response) {
       let body = JSON.parse(response);
       var len = body.collection.items.length;
-      // console.log(`Length is  ${len}`);
+      console.log(`Length is  ${len}`);
       var hrefArr = [];
-      let containerArray = [];
+      let objArray = [];
       for (var i = 0; i < body.collection.items.length; i++) {
         //console.log(`Iteration is ${i}`);
         if (body.collection.items[i].data[0].media_type === "image") {
@@ -109,59 +90,105 @@ $(document).ready(function () {
           var href = body.collection.items[i].links[0].href;
           var mediaType = body.collection.items[i].data[0].media_type;
           let container = new Container();
-          container.setter(dateCreated, description, nasaId, title, mediaType, href)
+          container.setter(dateCreated, description, nasaId, title, mediaType, href);
           // container.setter(dateCreated, description, nasaId, title, mediaType, href);
-          containerArray.push(container);
+          objArray.push(container);
           //console.log(`For ${i} I am inside & links is ${href}`);
         }
       }
 
-      var len2 = containerArray.length;
-      var k = 0;
+      var len2 = objArray.length;
+      var index = 0;
 
-      let htmlContent = `<figure><img src="${containerArray[k].href}" alt="${containerArray[k].description}" class="img-fluid"></figure>`;
-      $("#results").empty().append(htmlContent);
-      // $('.showTitle').empty().text(<span class="coralColor"> +"Title:" +</span> + containerArray[k].title);
-      $('.showTitle').empty().text(`Title: ${containerArray[k].title}`);
-      $('.showDateCreated').empty().text(`Date Created: ${containerArray[k].dateCreated}`);
-      $('.showDescription').empty().text(`Description: ${containerArray[k].description}`);
-      $('.showHref').empty().text(`Image URL: ${containerArray[k].href}`);
+      //let nasaContainer = new Container();
+      printMetaData(index, metaDataSelection, objArray[index]);
+      //console.log(`temp value is ${temp}, index is ${index}, metaDataSelection is ${metaDataSelection}, object array index is ${objArray[index]}`);
+     
+      // let htmlContent = `<figure><img src="${objArray[index].href}" alt="${objArray[index].description}" class="img-fluid"></figure>`;
+      // $("#results").empty().append(htmlContent);
+      // // $('.showTitle').empty().text(<span class="coralColor"> +"Title:" +</span> + objArray[index].title);
+      // $('.showTitle').empty().text(`Title: ${objArray[index].title}`);
+      // $('.showDateCreated').empty().text(`Date Created: ${objArray[index].dateCreated}`);
+      // $('.showDescription').empty().text(`Description: ${objArray[index].description}`);
+      // $('.showHref').empty().text(`Image URL: ${objArray[index].href}`);
 
 
       $('#next').on('click', function () {
-        k += 1;
-        if (k === len2 - 1) {
-          k = 0;
+        index += 1;
+        if (index === len2 - 1) {
+          index = 0;
         }
-        console.log(`inside next k is ${k}`);
-        let htmlContent = `<figure><img src="${containerArray[k].href}" alt="${containerArray[k].description}" class="img-fluid"></figure>`;
-        $("#results").empty().append(htmlContent);
-        $('.showTitle').empty().text(`Title: ${containerArray[k].title}`);
-        $('.showDateCreated').empty().text(`Date Created: ${containerArray[k].dateCreated}`);
-        $('.showDescription').empty().text(`Description: ${containerArray[k].description}`);
-        $('.showHref').empty().text(`Image URL: ${containerArray[k].href}`);
+        printMetaData(index, metaDataSelection, objArray[index]);
+        console.log(`inside next k is ${index}`);
+
       });
 
       $('#previous').on('click', function () {
-        k -= 1;
-        if (k === -1) {
-          k = len2 - 1;
+        index -= 1;
+        if (index === -1) {
+          index = len2 - 1;
         }
-        console.log(`inside previous k is ${k}`);
-        let htmlContent = `<figure><img src="${containerArray[k].href}" alt="${containerArray[k].description}" class="img-fluid"></figure>`;
-        $("#results").empty().append(htmlContent);
-
-        $('.showTitle').empty().text(`Title: ${containerArray[k].title}`);
-        $('.showDateCreated').empty().text(`Date Created: ${containerArray[k].dateCreated}`);
-        $('.showDescription').empty().text(`Description: ${containerArray[k].description}`);
-        $('.showHref').empty().text(`Image URL: ${containerArray[k].href}`);
+        printMetaData(index, metaDataSelection, objArray[index]);
+        console.log(`inside previous k is ${index}`);
       });
 
 
+      function printMetaData(index, checkBoxArray, objArrayElement) {
+        console.log(`inside createOuputContent: index is ${index} , checkBoxArray is ${checkBoxArray}, objArrayElement is ${objArrayElement}`);
+        $('.showTitle').empty();
+        $('.showDateCreated').empty();
+        $('.showDescription').empty();
+        $('.showHref').empty();
+        
+        let htmlContent = `<figure><img src="${objArray[index].href}" alt="${objArray[index].description}" class="img-fluid"></figure>`;
+        $("#results").empty().append(htmlContent);
+        if (checkBoxArray.length != 0) {
+            for (let i = 0; i < checkBoxArray.length; i++) {
+                if (checkBoxArray[i] === "title") {
+                  $('.showTitle').empty().text(`Title: ${objArray[index].title}`);                  
+                }
+                else if (checkBoxArray[i] === "dateCreated") {
+                  $('.showDateCreated').empty().text(`Date Created: ${objArray[index].dateCreated}`);
+                }
+                else if (checkBoxArray[i] === "description") {
+                  $('.showDescription').empty().text(`Description: ${objArray[index].description}`);
+                }
+                else if (checkBoxArray[i] === "href") {
+                  $('.showHref').empty().text(`Image URL: ${objArray[index].href}`);
+                }
+            }            
+        }
+       
+        let metaDataList = "";
+        var toppingList = $("ol#metaData2");
+        checkBoxArray.forEach(function (box) {
+          metaDataList += "<li><span class=\"coralColor\">" + box + "</span></li>";
+        });
+        console.log(`metaDataList is ${metaDataList}`);
+       toppingList.html(metaDataList);
+       let tempmetaDataList = "SOMETHING";
+     //  $("ol#metaData2").html("Hello <b>world</b>!");
+      // $('ol#metaData').html(checkBoxArray[0]);
 
 
+    
 
-      //<img src=containerArray[j].href alt="Girl in a jacket" style="width:500px;height:600px;"></img>
+
+      //   console.log(`metaDataList is ${metaDataList}`);
+      //  // $("#metaData").show(metaDataList);
+      //  $('.metaData').empty().text(`WHAT YOU REQUESTED`);
+      // // $("#metaData").show(metaDataList);
+      
+
+    
+
+
+      }
+
+
+  
+
+      //<img src=objArray[j].href alt="Girl in a jacket" style="width:500px;height:600px;"></img>
 
       //   $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
       //   $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
