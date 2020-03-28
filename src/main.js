09280fb4a4bf05ced58c4f5f231ad288d1a5c5bd
Salim Mayan <mailsalim@gmail.com>
2020-03-27 20:42:26 -0700

    Fixed - working again

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { Container } from './Container.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+//function to hide check boxes until right option is selected in Drop-down Menu
 $(function () {
   $('#cBId').hide();
   $('#dDId').change(function () {
@@ -13,6 +14,7 @@ $(function () {
   });
 });
 
+//function to disable Submit button until right option is selected in Drop-down Menu
 $("select#dDId").on('change', function () {
   let temp = $("#dDId option:selected").text();
   if (temp === "Select") {
@@ -25,10 +27,11 @@ $("select#dDId").on('change', function () {
 
 $(document).ready(function () {
   let buttonObj = document.querySelector("button");
+  //Reload Page upon clicking "Refresh" button
   $('#restart').click(function () {
     $('.accordion-item').hide();
     $(this).siblings("div").show();
-    document.location.reload(true);       //Reload Page
+    document.location.reload(true);
     buttonObj.textContent = "submit";
   });
   $('#formOne').submit(function () {
@@ -38,18 +41,15 @@ $(document).ready(function () {
     $('#showDescription').empty();
     $('#showHref').empty();
     $('#metaDataId').empty();
-    $("#metaData").hide();
-    $("#previous").show();
-    $("#next").show();
+    $("#previous").hide();
+    $("#next").hide();
     let userEnteredMetaData = $(".metaData").val();
-    console.log(`Main: userEnteredMetaData is ${userEnteredMetaData}`);
-    let metaDataSelection = [];
+    let clickedCheckBoxes = [];
     $("input:checkbox[name=metaData]:checked").each(function () {
-      metaDataSelection.push($(this).val());
+      clickedCheckBoxes.push($(this).val());
     });
     $("input[type=checkbox]").each(function () { this.checked = false; }); //to uncheck previously checked checkboxes
-    //console.log(`Main: metaDataSelection is ${metaDataSelection}`);
-
+    
     let promise = new Promise(function (resolve, reject) {
       //const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
       const url = `https://images-api.nasa.gov/search?q=apollo%2011...`;
@@ -62,14 +62,12 @@ $(document).ready(function () {
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
     });
-
     promise.then(function (response) {
       let body = JSON.parse(response);
-      let len = body.collection.items.length;
-      let hrefArr = [];
-      let objArray = [];
+      let mediaObjCount = body.collection.items.length;
+      let imgObjArray = [];
       for (let i = 0; i < body.collection.items.length; i++) {
         if (body.collection.items[i].data[0].media_type === "image") {
           let title = body.collection.items[i].data[0].title;
@@ -80,62 +78,65 @@ $(document).ready(function () {
           let mediaType = body.collection.items[i].data[0].media_type;
           let container = new Container();
           container.setter(dateCreated, description, nasaId, title, mediaType, href);
-          objArray.push(container);
+          imgObjArray.push(container);
         }
       }
+      let imgObjCount = imgObjArray.length;
+      console.log(`Objects:ImageObjects::${mediaObjCount}:${imgObjCount}`);
 
-      let len2 = objArray.length;
+      $("#previous").show();
+      $("#next").show();      
       let index = 0;
 
-      printMetaData(index, metaDataSelection, objArray[index]);
+      printMetaData(index, clickedCheckBoxes, imgObjArray[index]);
       $('#next').on('click', function () {
         index += 1;
-        if (index === len2 - 1) {
+        if (index === imgObjCount - 1) {
           index = 0;
         }
-        printMetaData(index, metaDataSelection, objArray[index]);
+        printMetaData(index, clickedCheckBoxes, imgObjArray[index]);
       });
 
       $('#previous').on('click', function () {
         index -= 1;
         if (index === -1) {
-          index = len2 - 1;
+          index = imgObjCount - 1;
         }
-        printMetaData(index, metaDataSelection, objArray[index]);
+        printMetaData(index, clickedCheckBoxes, imgObjArray[index]);
       });
 
-      function printMetaData(index, checkBoxArray, objArrayElement) {
+      function printMetaData(index, checkBoxArray, imgObjArrayElement) {
         $('#showTitle').empty();
         $('#showDateCreated').empty();
         $('#showDescription').empty();
         $('#showHref').empty();
         $('#metaDataId').empty();
-        let htmlContent = `<figure><img src="${objArray[index].href}" alt="${objArray[index].description}" class="img-fluid"></figure>`;
+        let htmlContent = `<figure><img src="${imgObjArrayElement.href}" alt="${imgObjArrayElement.description}" class="img-fluid"></figure>`;
         $("#results").empty().append(htmlContent);
-        if (checkBoxArray.length != 0) {
+        if (checkBoxArray.length != 0 && userEnteredMetaData === "With Meta Data") {
           for (let i = 0; i < checkBoxArray.length; i++) {
             if (checkBoxArray[i] === "title") {
-              let showTitleText = "<span class=\"coralColor\">Title: </span>" + objArray[index].title + "<br>";
+              let showTitleText = "<span class=\"coralColor\">Title: </span>" + imgObjArrayElement.title + "<br>";
               let showTitleTargetId = $("#showTitle");
               showTitleTargetId.html(showTitleText);
             }
             else if (checkBoxArray[i] === "dateCreated") {
-              let showDateCreateText = "<span class=\"coralColor\">Date Created: </span>" + objArray[index].dateCreated.substring(0, 10) + "<br>";
+              let showDateCreateText = "<span class=\"coralColor\">Date Created: </span>" + imgObjArrayElement.dateCreated.substring(0, 10) + "<br>";
               let showDateCreateTargetId = $("#showDateCreated");
               showDateCreateTargetId.html(showDateCreateText);
             }
             else if (checkBoxArray[i] === "description") {
-              let showDescriptionText = "<span class=\"coralColor\">Description: </span>" + objArray[index].description + "<br>";
+              let showDescriptionText = "<span class=\"coralColor\">Description: </span>" + imgObjArrayElement.description + "<br>";
               let showDescriptionTargetId = $("#showDescription");
               showDescriptionTargetId.html(showDescriptionText);
             }
             else if (checkBoxArray[i] === "href") {
-              let showHrefText = "<span class=\"coralColor\">Image URL: </span><a href=\"" + objArray[index].href + "\"target=\"_blank\">" + objArray[index].href + "</a>";
+              let showHrefText = "<span class=\"coralColor\">Image URL: </span><a href=\"" + imgObjArrayElement.href + "\"target=\"_blank\">" + imgObjArrayElement.href + "</a>";
               let showHrefTargetId = $("#showHref");
               showHrefTargetId.html(showHrefText);
             }
           }
-          let toppingList = $("p#metaDataId");
+          let metaDataIDer = $("p#metaDataId");
           let metaDataText;
           if (checkBoxArray.length === 1) {
             metaDataText = "<span class=\"coralColor\">Meta Data </span>element requested was ... <br>";
@@ -143,7 +144,7 @@ $(document).ready(function () {
           else if (checkBoxArray.length > 1) {
             metaDataText = "<span class=\"coralColor\">Meta Data </span>elements requested were ... <br>";
           }
-          toppingList.html(metaDataText);
+          metaDataIDer.html(metaDataText);
         }
       }
     });
